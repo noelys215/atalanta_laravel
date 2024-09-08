a429001d4b5082f65657f064d6a4d3b02ce90003
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-07 22:30:46 -0400

    chore: update logging.php

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,13 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'debug' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/debug.log'),
+            'level' => 'debug',
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
